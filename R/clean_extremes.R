0e3d5b3b5a35f2d1f52793d9f4cbc59d7e043ff2
SanchezCabeza <jasc2005@gmail.com>
2025-08-10 18:32:50 -0700

    Now working.

--- R/clean_extremes.R
+++ R/clean_extremes.R
@@ -97,10 +97,11 @@ clean_extremes <- function(data, column = "tem", n_check = 48, factor = 3) {
 
   # Remove rows safely
   remove_idx <- unique(na.omit(c(remove_start, remove_end)))
+  # delete unneeded columns
+  data <- data[ , 1:2] # revise for more variables
   if (length(remove_idx) == 0) {
     cleaned_data <- data
   } else {
-    cleaned_data <- data[ , 1:2] # revise for more variables
     cleaned_data <- data[-remove_idx, , drop = FALSE]
   }
 
