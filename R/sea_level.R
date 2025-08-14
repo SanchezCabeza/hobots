75dceb13c296fc1c4b533a353f343bf53b79b1ce
SanchezCabeza <jasc2005@gmail.com>
2025-08-13 19:13:46 -0700

    Avoid time tolerance

--- R/sea_level.R
+++ R/sea_level.R
@@ -17,8 +17,6 @@
 #'     \item \code{atm_pre} (numeric): Atmospheric pressure in kPa
 #'   }
 #' @param salinity Salinity in PSU (default = 35).
-#' @param join_tolerance Maximum allowed time difference between water and atmospheric
-#'   measurements when joining, given as a \code{\link[base]{difftime}} (default = \code{difftime(10, units = "mins")}).
 #'
 #' @return A data frame identical to \code{water_df} but with an additional
 #'   column \code{sl} giving the estimated water column height in meters.
@@ -29,76 +27,60 @@
 #' gauge pressure to meters of water using the water density at the given
 #' salinity and temperature.
 #'
-#' Water density is calculated following the UNESCO 1983 (EOS-80) equation
-#' of state for seawater.
-#'
 #' @examples
-#' \dontrun{
-#' water_df <- data.frame(
-#'   dateutc = as.POSIXct(c("2025-01-01 00:00:00", "2025-01-01 00:10:00"), tz = "UTC"),
-#'   pre = c(201.3, 201.5),
-#'   tem = c(25.0, 25.1)
-#' )
-#' atm_df <- data.frame(
-#'   dateutc = as.POSIXct(c("2025-01-01 00:00:00"), tz = "UTC"),
-#'   atm_pre = c(101.3)
-#' )
-#' calculate_sea_level(water_df, atm_df)
-#' }
+#' # water_df <- data.frame(dateutc = as.POSIXct(...), pre = ..., tem = ...)
+#' # atm_df <- data.frame(dateutc = as.POSIXct(...), atm_pre = ...)
+#' # result <- calc_sea_level(water_df, atm_df)
 #'
-#' @export
-sea_level <- function(water_df, atm_df, salinity = 35,
-                                join_tolerance = difftime(10, units = "mins")) {
-  # Ensure POSIXct
-  water_df$dateutc <- as.POSIXct(water_df$dateutc, tz = "UTC")
-  atm_df$dateutc <- as.POSIXct(atm_df$dateutc, tz = "UTC")
+calc_sea_level <- function(water_df, atm_df, salinity = 35) {
 
-  # Join water and atm data (nearest match within tolerance)
-  merged <- dplyr::left_join(
-    water_df,
-    dplyr::semi_join(
-      atm_df, atm_df,
-      by = character()
-    ),
-    by = character()
-  )
+  # Merge water and atmospheric data by exact timestamp
+  merged_df <- merge(water_df, atm_df, by = "dateutc", all.x = TRUE)
 
-  merged <- dplyr::left_join(
-    water_df,
-    dplyr::mutate(
-      atm_df,
-      dateutc_round = lubridate::round_date(dateutc, unit = "minute")
-    ),
-    by = c("dateutc" = "dateutc_round")
-  )
-
-  # Fill missing atm_pre with mean
-  mean_atm <- mean(atm_df$atm_pre, na.rm = TRUE)
-  merged$atm_pre[is.na(merged$atm_pre)] <- mean_atm
-
-  # Calculate density using UNESCO 1983 equation
-  sw_density <- function(S, T) {
-    # Pure water density at atmospheric pressure
-    rho_w <- 999.842594 + 6.793952e-2 * T - 9.095290e-3 * T^2 +
-      1.001685e-4 * T^3 - 1.120083e-6 * T^4 + 6.536332e-9 * T^5
-    # Seawater density at atmospheric pressure
-    A <- 0.824493 - 4.0899e-3 * T + 7.6438e-5 * T^2 -
-      8.2467e-7 * T^3 + 5.3875e-9 * T^4
-    B <- -5.72466e-3 + 1.0227e-4 * T - 1.6546e-6 * T^2
-    C <- 4.8314e-4
-    rho <- rho_w + A * S + B * S^(3/2) + C * S^2
-    return(rho) # kg/m^3
+  # If atmospheric pressure is missing, replace with mean atm pressure
+  if (any(is.na(merged_df$atm_pre))) {
+    mean_atm <- mean(merged_df$atm_pre, na.rm = TRUE)
+    merged_df$atm_pre[is.na(merged_df$atm_pre)] <- mean_atm
   }
 
-  # Gauge pressure (Pa) = (submerged absolute pressure - atm pressure) * 1000 (kPa → Pa)
-  merged$gauge_pressure <- (merged$pre - merged$atm_pre) * 1000
+  # Calculate density using UNESCO 1983 (EOS-80) equation
+  sw_density <- function(S, T) {
+    # Density of pure water at atmospheric pressure
+    a0 <- 999.842594
+    a1 <- 6.793952e-2
+    a2 <- -9.095290e-3
+    a3 <- 1.001685e-4
+    a4 <- -1.120083e-6
+    a5 <- 6.536332e-9
+    rho_w <- a0 + (a1 + (a2 + (a3 + (a4 + a5 * T) * T) * T) * T) * T
 
-  # Water density
-  merged$density <- sw_density(salinity, merged$tem)
+    # Seawater density at atmospheric pressure
+    b0 <- 0.824493
+    b1 <- -4.0899e-3
+    b2 <- 7.6438e-5
+    b3 <- -8.2467e-7
+    b4 <- 5.3875e-9
+    c0 <- -5.72466e-3
+    c1 <- 1.0227e-4
+    c2 <- -1.6546e-6
+    d0 <- 4.8314e-4
 
-  # Sea level (m) = gauge pressure / (density * g)
+    rho_sw <- rho_w +
+      (b0 + (b1 + (b2 + (b3 + b4 * T) * T) * T) * T) * S +
+      (c0 + (c1 + c2 * T) * T) * S^(3/2) +
+      d0 * S^2
+
+    return(rho_sw) # kg/m^3
+  }
+
+  # Calculate density for each row
+  rho <- sw_density(salinity, merged_df$tem)
+
+  # Convert kPa to m of water: 1 kPa = 1000 N/m²
+  # Water column height h = (P_sub - P_atm) / (rho * g)
   g <- 9.80665
-  merged$sl <- merged$gauge_pressure / (merged$density * g)
+  merged_df$sl <- ( (merged_df$pre - merged_df$atm_pre) * 1000 ) / (rho * g)
 
-  return(merged)
+  return(merged_df)
 }
+
